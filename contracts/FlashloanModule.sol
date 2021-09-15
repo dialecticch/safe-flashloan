0daf6046b22aa1933de105009ae00202a9bf9883
decanus <7621705+decanus@users.noreply.github.com>
2021-09-15 15:19:56 +0200

    fee

--- contracts/FlashloanModule.sol
+++ contracts/FlashloanModule.sol
@@ -8,6 +8,14 @@ import "./LoanModule.sol";
 /// @dev A simple Module allowing anyone to execute Flashloans using the safe.
 /// @author Dialectic
 contract FlashloanModule is LoanModule {
+
+    /// The fees for a given safe.
+    mapping (address => uint256) public fee;
+
+    function setFee(uint256 fee) {
+        fee[msg.sender] = fee;
+    }
+
     function execute(
         GnosisSafe safe,
         address[] calldata assets,
@@ -18,9 +26,12 @@ contract FlashloanModule is LoanModule {
         require(amounts.length == assets.length, "Length does not match");
 
         uint256[] memory outputs = new uint256[](amounts.length);
+
+        uint256 safeFee = fee[address(safe)];
+
         for (uint256 i = 0; i < assets.length; i++) {
             transfer(safe, msg.sender, assets[i], amounts[i]);
-            outputs[i] = amounts[i]; // @TODO CALC FEE
+            outputs[i] = (amounts[i] * safeFee) / 10000;
         }
 
         FlashloanReceiver(to).flash(safe, assets, amounts, outputs, data);
